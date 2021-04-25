729c11c8f9c7bc150ed07c85d69fc2f4a6c8c027
anastiu6a <43859330+anastiu6a@users.noreply.github.com>
2021-04-25 18:50:48 +0300

    New Helper clases added

---
+++ addressbook-web-tests/src/test/java/ru/lessons/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,44 @@
+package ru.lessons.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import ru.lessons.addressbook.model.ContactData;
+
+public class ContactHelper {
+  private WebDriver wd;
+  public ContactHelper (WebDriver wd) {
+    this.wd = wd;
+  }
+
+  public void returnToHomepage() {
+
+    wd.findElement(By.linkText("home")).click();
+  }
+
+  public void submitContactCreation() {
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  public void fillContactForm(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+  }
+
+  public void initContactCreation() {
+
+    wd.findElement(By.linkText("add new")).click();
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/lessons/addressbook/tests/GroupDeletionTests.java
@@ -0,0 +1,17 @@
+package ru.lessons.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().deleteSelectedGroup();
+    app.getGroupHelper().returnToGroupPage();
+  }
+
+
+}
