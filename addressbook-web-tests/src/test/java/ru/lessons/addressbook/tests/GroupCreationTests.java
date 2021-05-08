7b0b5c083edb8f8405e370f060f2707b120e58da
anastiu6a <43859330+anastiu6a@users.noreply.github.com>
2021-05-08 14:02:03 +0300

    Fill form optimization process

--- addressbook-web-tests/src/test/java/ru/lessons/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/lessons/addressbook/tests/GroupCreationTests.java
@@ -9,7 +9,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("Test0", "Test1", "Test2"));
+    app.getGroupHelper().fillGroupForm(new GroupData("Test0", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
   }
