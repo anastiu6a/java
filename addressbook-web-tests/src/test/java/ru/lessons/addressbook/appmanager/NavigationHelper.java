8b86cf2447665bbb4d167d2db544192e5584c036
anastiu6a <43859330+anastiu6a@users.noreply.github.com>
2021-05-13 21:50:01 +0300

    added new method (isElementPresent) to Base Helper

--- addressbook-web-tests/src/test/java/ru/lessons/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/lessons/addressbook/appmanager/NavigationHelper.java
@@ -9,9 +9,20 @@ public class NavigationHelper extends BaseHelper {
     super(wd);
   }
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new")))
+    {
+      return;
+    }
     click(By.linkText("groups"));
   }
+
+
   public void returnToHomepage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.linkText("home"));
   }
 }
