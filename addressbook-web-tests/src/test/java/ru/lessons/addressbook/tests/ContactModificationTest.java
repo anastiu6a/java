e0ef6c5d62532616d32f7910b17cc939c52b3655
anastiu6a <43859330+anastiu6a@users.noreply.github.com>
2021-05-25 19:40:39 +0300

    Fixed issue with modification test

--- addressbook-web-tests/src/test/java/ru/lessons/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/lessons/addressbook/tests/ContactModificationTest.java
@@ -19,8 +19,7 @@ public class ContactModificationTest extends TestBase {
     }
     app.getNavigationHelper().returnToHomepage();
     List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().selectContact(before.size() - 1);
-    app.getContactHelper().initContactModification();
+    app.getContactHelper().initContactModification(before.size() - 1);
     ContactData contact = new ContactData(before.get(before.size() -1).getId(), "test1", "test2","test3","test4", "test5");
     app.getContactHelper().fillContactForm(contact);
     app.getContactHelper().submitContactModification();
